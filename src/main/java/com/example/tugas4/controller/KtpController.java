37950339132feea5dfbe6f1d9ac13bf95c95072a
Asyurazhr <Asyuradjola2004@gmail.com>
2026-07-15 20:35:03 +0700

    add public class KtpController

---
+++ src/main/java/com/example/tugas4/controller/KtpController.java
@@ -0,0 +1,19 @@
+package com.example.tugas4.controller;
+
+
+
+import com.example.tugas4.dto.KtpDto;
+import com.example.tugas4.entity.KtpEntity;
+import com.example.tugas4.service.KtpService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/ktp")
+@CrossOrigin(origins = "*") // Supaya bisa diakses dari file HTML (Frontend)
+public class KtpController {
+
+}

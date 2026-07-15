1c238baf2836f30192e6ba01d059f6c8caed5b14
Asyurazhr <Asyuradjola2004@gmail.com>
2026-07-15 20:39:02 +0700

    add nomorktp, nama lengkap, alamat, tanggal lahir dan jenis kelamin

--- src/main/java/com/example/tugas4/dto/KtpDto.java
+++ src/main/java/com/example/tugas4/dto/KtpDto.java
@@ -5,4 +5,9 @@ import lombok.Data;
 
 @Data
 public class KtpDto {
+    private String nomorKtp;
+    private String namaLengkap;
+    private String alamat;
+    private String tanggalLahir;
+    private String jenisKelamin;
 }
